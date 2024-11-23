9e2932036bf2188217a5527ce82f089079d694f9
Jam Batista <batistadado@gmail.com>
2024-11-23 14:31:26 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
 function navigateTo(page) {
   window.location.href = page;
 }
+
+// Seleciona todos os spans dentro do h1
+const spans = document.querySelectorAll('.titulo-animado span');
+
+// Adiciona um delay de animação para cada letra
+spans.forEach((span, index) => {
+  span.style.animationDelay = `${index * 0.1}s`;
+});
